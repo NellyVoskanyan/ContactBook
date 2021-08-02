878e16fd10c8029a6e6004a785f42d9803fc8612
Nelli Voskanyan <voskanyan74@inbox.ru>
2021-08-02 15:11:02 +0400

    Add project files.

---
+++ ContactBook/Data/ContactBookContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContactBook.Models;
+
+namespace ContactBook.Data
+{
+    public class ContactBookContext : DbContext
+    {
+        public ContactBookContext (DbContextOptions<ContactBookContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<ContactBook.Models.Contact> Contact { get; set; }
+    }
+}
---
+++ ContactBook/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactBook.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+
+
+
+    }
+}
